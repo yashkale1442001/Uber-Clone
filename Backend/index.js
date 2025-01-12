ce991db4db7deef4c966c3153d1407b2cd58e603
Yash Kale <yashraj3412y@gmail.com>
2025-01-12 12:12:59 +0530

    User Registration and Login done

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
-const dotenv= require('dotenv')
-const express= require('express')
-const cors= require('cors')
+const dotenv= require('dotenv');
+const express= require('express');
+const cors= require('cors');
 
 dotenv.config();
 const app= express();
